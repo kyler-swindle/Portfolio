d055c264ab1a548906164a3aa9a867356723a905
kyler-swindle <166889571+kyler-swindle@users.noreply.github.com>
2024-04-13 05:59:20 -0500

    Update 1 Game.java
    added more methods.
    most notably checkHand method (still WIP).

--- Holdem/Game.java
+++ Holdem/Game.java
@@ -1,9 +1,9 @@
 /***** 
    Texas Hold'em Project 
    
-   Date Started : Friday, April 12, 2024
+   Date Started : Friday, April 12, 2024, 19:15
    
-   Date Finished : xxxday, XXX XX, XXXX
+   Date Finished : xxxday, XXX XX, XXXX, XX:XX
    
    Made by : Kyler A. Swindle (Auburn University - Software Engineering Major - Sophomore)
       Contact : 
@@ -25,6 +25,7 @@ class Game {
    public static void main (String[] args) {
       Table game = new Table(5);
       
+      System.out.println("\nPlayers: ");
       for (int i = 0; i < game.players.size(); i++) {
          for (int j = 0; j < 2; j++) {
             if (j == 0) {
@@ -32,10 +33,95 @@ class Game {
                System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ", ");
             } else {
                System.out.print(game.players.get(i).getPocket().get(j) + ":");
-               System.out.println(game.decodeCard(game.players.get(i).getPocket().get(j)) + ".");
+               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ".");
             }
          }
+         
+         System.out.println("Hand: " + game.decodeHand(game.players.get(i).getHand()));
       }
+      
+      System.out.println("\nFlop: ");
+      game.flop();
+      for (int i = 0; i < game.table.size(); i++) {
+            if (i == game.table.size() - 1) {
+               System.out.print(game.table.get(i) + ":");
+               System.out.println(game.decodeCard(game.table.get(i)) + ". ");
+            } else {
+               System.out.print(game.table.get(i) + ":");
+               System.out.print(game.decodeCard(game.table.get(i)) + ", ");
+            }
+      }
+      
+      System.out.println("\nPlayers: ");
+      for (int i = 0; i < game.players.size(); i++) {
+         for (int j = 0; j < 2; j++) {
+            if (j == 0) {
+               System.out.print(game.players.get(i).getPocket().get(j) + ":");
+               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ", ");
+            } else {
+               System.out.print(game.players.get(i).getPocket().get(j) + ":");
+               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ".");
+            }
+         }
+         
+         System.out.println("Hand: " + game.decodeHand(game.players.get(i).getHand()));
+      }
+      
+      System.out.println("\nTurn: ");
+      game.turn();
+      for (int i = 0; i < game.table.size(); i++) {
+            if (i == game.table.size() - 1) {
+               System.out.print(game.table.get(i) + ":");
+               System.out.println(game.decodeCard(game.table.get(i)) + ". ");
+            } else {
+               System.out.print(game.table.get(i) + ":");
+               System.out.print(game.decodeCard(game.table.get(i)) + ", ");
+            }
+      }
+      
+      System.out.println("\nPlayers: ");
+      for (int i = 0; i < game.players.size(); i++) {
+         for (int j = 0; j < 2; j++) {
+            if (j == 0) {
+               System.out.print(game.players.get(i).getPocket().get(j) + ":");
+               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ", ");
+            } else {
+               System.out.print(game.players.get(i).getPocket().get(j) + ":");
+               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ".");
+            }
+         }
+         
+         System.out.println("Hand: " + game.decodeHand(game.players.get(i).getHand()));
+      }
+      
+      System.out.println("\nRiver: ");
+      game.river();
+      for (int i = 0; i < game.table.size(); i++) {
+            if (i == game.table.size() - 1) {
+               System.out.print(game.table.get(i) + ":");
+               System.out.println(game.decodeCard(game.table.get(i)) + ". ");
+            } else {
+               System.out.print(game.table.get(i) + ":");
+               System.out.print(game.decodeCard(game.table.get(i)) + ", ");
+            }
+      }
+      
+      System.out.println("\nPlayers: ");
+      for (int i = 0; i < game.players.size(); i++) {
+         for (int j = 0; j < 2; j++) {
+            if (j == 0) {
+               System.out.print(game.players.get(i).getPocket().get(j) + ":");
+               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ", ");
+            } else {
+               System.out.print(game.players.get(i).getPocket().get(j) + ":");
+               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ".");
+            }
+         }
+         
+         System.out.println("Hand: " + game.decodeHand(game.players.get(i).getHand()));
+      }
+      
+      System.out.println("\nHighest Hand: Player:" + game.currHighestHand() + " - " + game.decodeHand(game.players.get(game.currHighestHand()).getHand()));
    }
    
 }
