34b3dcd91ee886a189ca0bde92c9a3baa83baa30
kyler-swindle <166889571+kyler-swindle@users.noreply.github.com>
2024-04-16 00:10:56 -0500

    Update 5 Game.java
    removed print statements and relocated them into methods in Table class.

--- Holdem/Game.java
+++ Holdem/Game.java
@@ -33,106 +33,14 @@ class Game {
 
    public static void main (String[] args) {
       Table game = new Table(5);
-            
-      System.out.println("\nPlayers: ");
-      for (int i = 0; i < game.players.size(); i++) {
-         for (int j = 0; j < 2; j++) {
-            if (j == 0) {
-               System.out.print(game.players.get(i).getPocket().get(j) + ":");
-               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ", ");
-            } else {
-               System.out.print(game.players.get(i).getPocket().get(j) + ":");
-               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ".");
-            }
-         }
-         
-         System.out.println(" Hand: " + game.decodeHand(game.players.get(i).getHand()));
-      }
       
-      System.out.println("\nFlop: ");
+      game.printHands();
+      
       game.flop();
-      for (int i = 0; i < game.table.size(); i++) {
-            if (i == game.table.size() - 1) {
-               System.out.print(game.table.get(i) + ":");
-               System.out.println(game.decodeCard(game.table.get(i)) + ". ");
-            } else {
-               System.out.print(game.table.get(i) + ":");
-               System.out.print(game.decodeCard(game.table.get(i)) + ", ");
-            }
-      }
-            
-      System.out.println("\nPlayers: ");
-      for (int i = 0; i < game.players.size(); i++) {
-         for (int j = 0; j < 2; j++) {
-            if (j == 0) {
-               System.out.print(game.players.get(i).getPocket().get(j) + ":");
-               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ", ");
-            } else {
-               System.out.print(game.players.get(i).getPocket().get(j) + ":");
-               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ".");
-            }
-         }
-         
-         System.out.println(" Hand: " + game.decodeHand(game.players.get(i).getHand()));
-      }
-      
-      System.out.println("\nTurn: ");
       game.turn();
-      for (int i = 0; i < game.table.size(); i++) {
-            if (i == game.table.size() - 1) {
-               System.out.print(game.table.get(i) + ":");
-               System.out.println(game.decodeCard(game.table.get(i)) + ". ");
-            } else {
-               System.out.print(game.table.get(i) + ":");
-               System.out.print(game.decodeCard(game.table.get(i)) + ", ");
-            }
-      }
-      
-      System.out.println("\nPlayers: ");
-      for (int i = 0; i < game.players.size(); i++) {
-         for (int j = 0; j < 2; j++) {
-            if (j == 0) {
-               System.out.print(game.players.get(i).getPocket().get(j) + ":");
-               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ", ");
-            } else {
-               System.out.print(game.players.get(i).getPocket().get(j) + ":");
-               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ".");
-            }
-         }
-         
-         System.out.println(" Hand: " + game.decodeHand(game.players.get(i).getHand()));
-      }
-      
-      System.out.println("\nRiver: ");
       game.river();
-      for (int i = 0; i < game.table.size(); i++) {
-            if (i == game.table.size() - 1) {
-               System.out.print(game.table.get(i) + ":");
-               System.out.println(game.decodeCard(game.table.get(i)) + ". ");
-            } else {
-               System.out.print(game.table.get(i) + ":");
-               System.out.print(game.decodeCard(game.table.get(i)) + ", ");
-            }
-      }
-      
-      System.out.println("\nPlayers: ");
-      for (int i = 0; i < game.players.size(); i++) {
-         for (int j = 0; j < 2; j++) {
-            if (j == 0) {
-               System.out.print(game.players.get(i).getPocket().get(j) + ":");
-               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ", ");
-            } else {
-               System.out.print(game.players.get(i).getPocket().get(j) + ":");
-               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ".");
-            }
-         }
-         
-         System.out.println(" Hand: " + game.decodeHand(game.players.get(i).getHand()));
-      }
-      
-      System.out.println("\nHighest Hand: Player:" + game.currHighestHand() + " - " + game.decodeHand(game.players.get(game.currHighestHand()).getHand()) + " (" + game.players.get(game.currHighestHand()).getHand() + ")");
-      
-      /*
+            
+      /* DEBUGGING
       game.removePlayers(new ArrayList(Arrays.asList(0,1,2,3)));
       
       
@@ -150,39 +58,6 @@ class Game {
       game.players.get(3).setPocket(new ArrayList(Arrays.asList(21, 26)));
       game.players.get(4).setPocket(new ArrayList(Arrays.asList(19, 48)));
       game.postCard();
-      
-      for (int i = 0; i < game.table.size(); i++) {
-            if (i == game.table.size() - 1) {
-               System.out.print(game.table.get(i) + ":");
-               System.out.println(game.decodeCard(game.table.get(i)) + ". ");
-            } else {
-               System.out.print(game.table.get(i) + ":");
-               System.out.print(game.decodeCard(game.table.get(i)) + ", ");
-            }
-      }
-      
-      System.out.println("\nPlayers: ");
-      for (int i = 0; i < game.players.size(); i++) {
-         for (int j = 0; j < 2; j++) {
-            if (j == 0) {
-               System.out.print(game.players.get(i).getPocket().get(j) + ":");
-               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ", ");
-            } else {
-               System.out.print(game.players.get(i).getPocket().get(j) + ":");
-               System.out.print(game.decodeCard(game.players.get(i).getPocket().get(j)) + ".");
-            }
-         }
-         
-         System.out.println(" Hand: " + game.decodeHand(game.players.get(i).getHand()));
-      }
-      
-      System.out.println("\nHighest Hand: Player:" + game.currHighestHand() + " - " + game.decodeHand(game.players.get(game.currHighestHand()).getHand()));
-      
-      
-      for (int i = 0; i < 52; i++) {
-         // System.out.println(i + ":" + i % 4 + ":" + i / 4 + ":" + i / 13 + ":" + i % 13);
-         System.out.println(i + ":" + i / 4+ ":" + game.decodeCard(i));
-      }
       */
    }
 }
